b88932ca5c240aa501635a7ee1ea309702547dea
kkunh97@naver.com <kkunh97@naver.com>
2024-01-27 11:32:04 +0900

    Initial Commit

---
+++ cppTheory/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppTheory
+//
+//  Created by 김건혁 on 1/27/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
